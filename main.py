24e0dae6dff20c4cbf7ed76ef3b5a73b1793823c
Noxan <59417659+noxan-dev@users.noreply.github.com>
2022-02-27 19:43:42 -0500

    More changes to the game calculation and display

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-import random
-import time
 import datetime
+import random
 import threading
+import time
 import PySimpleGUI as sg
 
-speed = 0
+wpm = 0
 EASY_WORD_COUNT = 1
 EASY_TIMER = 60
 
-minutes, seconds = (0, 1)
+minutes, seconds = (0, EASY_TIMER)
 timer_format = datetime.timedelta(minutes=minutes, seconds=seconds)
 
-words = []
 sg.theme('DarkBlue2')
 sg.set_options(font=("Courier New", 10))
 
-layout = [[sg.Text("Start typing to track speed"), sg.Text(f"Speed: {speed}/WPM", auto_size_text=True, key="-SPEED-"),
+layout = [[sg.Text("Start typing to track speed"), sg.Text(f"Speed: {wpm}/WPM", auto_size_text=True, key="-SPEED-"),
            sg.Text(f"Time Left {timer_format}: ", key="-TIMER-")],
           [sg.Text('Start', auto_size_text=True, text_color='black', key="-WORDS-")],
           [sg.Multiline("", size=(30, 10), no_scrollbar=True, key='-TEXT-')],
@@ -23,13 +22,16 @@ layout = [[sg.Text("Start typing to track speed"), sg.Text(f"Speed: {speed}/WPM"
 
 
 def countdown(min, sec):
+    global wpm
     timer = datetime.timedelta(seconds=sec, minutes=min)
     while timer.seconds > 0:
         time.sleep(1)
         timer -= datetime.timedelta(seconds=1)
         window["-TIMER-"].Update(str(timer))
         if timer.seconds == 0:
-            sg.popup_ok(f'Here are your results: {speed}/WPM', title="Time's up!")
+            wpm = len(completed_words) - missed_words
+            print(len(completed_words))
+            sg.popup_ok(f'Here are your results: {wpm}/WPM', title="Time's up!")
     window["-TIMER-"].Update("0:00:00")
     time.sleep(1)
     return 0
@@ -37,6 +39,8 @@ def countdown(min, sec):
 
 window = sg.Window("Speed Typing Test", layout, element_justification='center',
                    return_keyboard_events=True, use_default_focus=False)
+
+words = []
 
 
 def random_word_gen():
@@ -53,6 +57,7 @@ do_not_listen = ['BackSpace:8', 'Tab:9', 'Return:13', 'Shift:16', 'Control_L:17'
                  'Up:38', 'MouseWheel:Up', 'MouseWheel:Down', 'Escape:27', '__TIMEOUT__']
 
 completed_words = []
+missed_words = 0
 # ---===--- Loop taking in user input --- #
 while True:
     event, values = window.read()
@@ -64,7 +69,7 @@ while True:
         print(words)
         break
     if len(event) > 1:
-        if event not in do_not_listen:
+        if event not in do_not_listen and event not in completed_words:
             text_elem.update(value=str(event)[0])
     if ord(str(event)[0]) == 32:
         if values['-TEXT-'].lower() == 'start':
@@ -74,11 +79,12 @@ while True:
         else:
             completed_words.append(values['-TEXT-'].split(' '))
             text_elem.update(value='')
-            window['-WORDS-'].update(completed_words + random_word_gen())
+            window['-WORDS-'].update(f'Last word: {completed_words[-1]}\nNew Word: {random_word_gen()}')
             for word in completed_words:
                 if word in words:
                     window['-WORDS-'].update(text_color='green')
                 else:
+                    missed_words += 1
                     window['-WORDS-'].update(text_color='red')
 
 window.close()
