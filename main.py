38d11547b177ab98c94a70f8eb173c889bbfcf97
Noxan <59417659+noxan-dev@users.noreply.github.com>
2022-02-28 13:42:06 -0500

    added scoring system

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import time
 import PySimpleGUI as sg
 
 wpm = 0
-missed_words = 0
 EASY_WORD_COUNT = 1
 EASY_TIMER = 60
 
@@ -15,7 +14,7 @@ timer_format = datetime.timedelta(minutes=minutes, seconds=seconds)
 sg.theme('DarkBlue2')
 sg.set_options(font=("Courier New", 10))
 
-layout = [[sg.Text("Start typing to track speed"), sg.Text(f"Speed: {wpm}/WPM", auto_size_text=True, key="-SPEED-"),
+layout = [[sg.Text("Start typing to track speed"),
            sg.Text(f"Time Left {timer_format}: ", key="-TIMER-")],
           [sg.Text("", size=(20, 1), auto_size_text=True, key="-OUTPUT-")],
           [sg.Text('Start', auto_size_text=True, text_color='black', key="-WORDS-")],
@@ -31,7 +30,7 @@ def countdown(min, sec):
         timer -= datetime.timedelta(seconds=1)
         window["-TIMER-"].Update(str(timer))
         if timer.seconds == 0:
-            wpm = len(completed_words) - missed_words
+            wpm = abs(len(completed_words) - missed_words)
             sg.popup_ok(f'Here are your results: {wpm}/WPM', title="Time's up!")
     window["-TIMER-"].Update("0:00:00")
     time.sleep(1)
@@ -48,9 +47,9 @@ def random_word_gen():
     with open('words') as f:
         data = f.readlines()
         random_words = random.choices(data, k=EASY_WORD_COUNT)
-        random_words = [random_word.strip() for random_word in random_words]
-        words.append(random_words)
-    return random_words
+        for random_word in random_words:
+            words.append(random_word.strip())
+    return random_word
 
 
 do_not_listen = ['BackSpace:8', 'Tab:9', 'Return:13', 'Shift:16', 'Control_L:17', 'Alt:18', 'PauseBreak:19',
@@ -61,24 +60,25 @@ completed_words = []
 
 # ---===--- Loop taking in user input --- #
 while True:
+    missed_words = 0
     event, values = window.read()
-    print(event, values)
     text_elem = window['-TEXT-']
     if event in ("OK", None):
         print(event, "exiting")
-        print(completed_words)
-        print(words)
         break
     if len(event) > 1:
-        if event not in do_not_listen and event not in completed_words:
+        if event not in do_not_listen:
             text_elem.update(value=str(event)[0])
+
     if ord(str(event)[0]) == 32 or ord(str(event)[0]) == 13:
-        if values['-TEXT-'].lower() == 'start':
+        if values['-TEXT-'] in completed_words:
+            sg.popup_ok(f'You already typed {values["-TEXT-"]}', title="Error")
+        elif values['-TEXT-'].lower() == 'start':
             thread_timer = threading.Thread(target=countdown, args=(minutes, seconds), daemon=True).start()
             text_elem.update(value='')
             window['-WORDS-'].update(random_word_gen())
         else:
-            completed_words.append(values['-TEXT-'].split(' '))
+            completed_words.append(values['-TEXT-'].split(' ')[-1])
             text_elem.update(value='')
             window['-WORDS-'].update(f'New Word: {random_word_gen()}')
             window['-OUTPUT-'].update(f'Last word: {completed_words[-1]}')
@@ -86,7 +86,7 @@ while True:
                 if word in words:
                     window['-OUTPUT-'].update(text_color='green')
                 else:
-                    missed_words += 1
                     window['-OUTPUT-'].update(text_color='red')
+                    missed_words += 1
 
 window.close()
